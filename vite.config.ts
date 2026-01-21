d151e2d58aa04ea667e888a5f3acbf31c29beabf
Toymakerftw <anandhraman@sngce.ac.in>
2026-01-21 19:15:07 +0530

    feat: replace PWA and favicon with new custom 'dumbbell.svg' icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,19 +18,9 @@ export default defineConfig({
         theme_color: '#020617',
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'dumbbell.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
